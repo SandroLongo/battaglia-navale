06f35ad85ebce8479424eb97123ae2a8b6599c39
SandroLongo <aless.longo03@gmail.com>
2024-10-22 16:31:52 +0200

    ho copiato nel file main.c il codice funzionante e compilabile della main2.c cosi diventa la main ufficicale

--- client/main.c
+++ client/main.c
@@ -3,103 +3,83 @@
 #include <WinSock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
-#include <Windows.h>
+#include <string.h>
+#include <stdlib.h>
 #pragma comment(lib, "ws2_32.lib")
 
-//definizione costanti
 #define PORTNUMBER 25123
 #define BUF_DIM 512
 
-const char* host = "192.168.1.1"; //"LAPTOP-IJ698CFS"; //https://www.whatismyip.com/ 
-
-//aleip : 192.168.1.1
+//aleip : 192.168.1.106
 // ip seli 87.20.233.29
 /*
 typedef struct casella {
-	unsigned char status; // presenza di acqua o nave (1 presenza di una parte di nave, 0 altrimenti)
-	unsigned char hit;// nave colpita o meno (1 per colpita, 0 altrimenti)
+    unsigned char status; // presenza di acqua o nave (1 presenza di una parte di nave, 0 altrimenti)
+    unsigned char hit;// nave colpita o meno (1 per colpita, 0 altrimenti)
 };
 
 typedef struct griglia {
-	casella[10][10] disp; //rappresentazione della griglia come matrice di caselle
+    casella[10][10] disp; //rappresentazione della griglia come matrice di caselle
 };
 */
 
 int main() {
-	//creazione connessione (include  accesso univoco utente  )
+    WSADATA wsaData;
+    SOCKET ds_sock;
+    struct sockaddr_in server_addr;
+    char buff[BUF_DIM];
+    char buf[BUF_DIM];
 
+    // Inizializzazione di Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData)) {
+        printf("Errore nella WSAStartup\n");
+        return 1;
+    }
 
-	WSADATA wsaData;
-	SOCKET ds_sock;
-	int lenght, res;
-	struct sockaddr_in client;
-	struct hostent* hp;
-	char buff[BUF_DIM];
-	char buf[BUF_DIM];
-	unsigned long server_addr;
+    // Creazione del socket
+    ds_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (ds_sock == INVALID_SOCKET) {
+        printf("Errore nella creazione del socket\n");
+        WSACleanup();
+        return 1;
+    }
 
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData))
-	{
-		printf("errore nella WSAStartup\n");
-		exit(EXIT_FAILURE);
-	}
-	ds_sock = socket(AF_INET, SOCK_STREAM, 0);
-	client.sin_family = AF_INET;
-	client.sin_port = htons(PORTNUMBER);  //da modificare o (da rivedere) tra 10 anni
-	//client.sin_addr.s_addr = INADDR_ANY;
-	//hp = gethostbyname("");
-	server_addr = inet_addr(host);
-	printf("il server si trova nell'indirizzo ip : %ul\n", server_addr);
-	//memcpy(hp->h_addr, &client.sin_addr, hp->h_length);
-	memcpy((void*)server_addr, &client.sin_addr.s_addr, sizeof(server_addr));
+    // Impostazione dell'indirizzo del server
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORTNUMBER);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  // Modifica l'indirizzo se necessario
 
-	printf("inizio della connessione\n");
-	fflush(stdout);
+    // Connessione al server
+    if (connect(ds_sock, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        printf("Errore nella connect\n");
+        closesocket(ds_sock);
+        WSACleanup();
+        return 1;
+    }
 
-	res = connect(ds_sock, (SOCKADDR*)&client, sizeof(client));
-	if (res == -1) {
-		printf("Errore nella connect \n");
-		fflush(stdout);
-		exit(1);
-	}
+    printf("Connessione effettuata al server\n");
 
-	printf("connesione effettuata\n");
-	printf("scrivere messaggi\n");
-	do {
-		scanf_s("%s", buff);
-		send(ds_sock, buff, BUF_DIM, 0);
-		recv(ds_sock, buf, BUF_DIM, 0);
-		printf("%s\n", buf);
-	} while (strcmp(buf, "quit") != 0);
+    // Ciclo di invio e ricezione dei messaggi
+    do {
+        printf("Inserisci un messaggio (o 'quit' per terminare): ");
+        fgets(buff, BUF_DIM, stdin);
+        buff[strcspn(buff, "\n")] = '\0';  // Rimuovi il newline
 
-	closesocket(ds_sock);
+        // Invia il messaggio al server
+        send(ds_sock, buff, strlen(buff), 0);
 
+        //  risposta dal server
+        int bytes_received = recv(ds_sock, buf, BUF_DIM, 0);
+        if (bytes_received > 0) {
+            buf[bytes_received] = '\0';  // Assicurati di terminare la stringa
+            printf("Risposta dal server: %s\n", buf);
+        }
+    } while (strcmp(buff, "quit") != 0);
 
+    // Chiusura del socket
+    closesocket(ds_sock);
+    WSACleanup();
 
-
-
-
-	/******
-	crezione/ricerca lobby
-
-	1-selezionare ricerca o creazione
-	se creazione
-	2)inserimento nome
-
-	***********/
-
-	/**********
-	GESTIONE PARTITA
-
-	1-disposizione navi
-	2-gestione turni
-	3-inserimenti e valutazione mossa
-	4-terminazione partita
-
-	*************/
-
-	WSACleanup(); // alla fine
-
-
-
+    return 0;
 }
