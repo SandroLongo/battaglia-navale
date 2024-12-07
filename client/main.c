df94ca9affd20a7f992fc1946857a707c57408be
SandroLongo <aless.longo03@gmail.com>
2024-12-07 20:27:12 +0100

    asdf

--- client/main.c
+++ client/main.c
@@ -229,7 +229,7 @@ SOCKET socket_connection()
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORTNUMBER);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  // Modifica l'indirizzo se necessario 192.168.1.106 "127.0.0.1"
+    server_addr.sin_addr.s_addr = inet_addr("192.168.1.106");  // Modifica l'indirizzo se necessario 192.168.1.106 "127.0.0.1"
 
     // Connessione al server
     if (connect(ds_sock, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
